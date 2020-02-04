c27915045d630610f3963892715ea37c4168a047
takamasa <taka@takamasas-MacBook-Air.local>
2020-02-04 21:22:54 +0900

    seventh commit

--- tweet_analysis/settings.py
+++ tweet_analysis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-(_dpjgi)n(a4a*--mj-a)t-rck@-+#4uqipbs1gcno(vb2s_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
