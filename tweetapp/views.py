9e110f734e129b28b8f0e72c8d1fc37bdda2e823
takamasa <taka@takamasas-MacBook-Air.local>
2020-02-04 20:46:13 +0900

    fix get_tweet.py

--- tweetapp/views.py
+++ tweetapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from get_tweet_data import get_tweets
+# from get_tweet_data import get_tweets
 
 # Create your views here.
 
@@ -9,7 +9,8 @@ def index(request):
 
 def get_tweet_id(request):
     tw_id = request.POST["tweet_id"]
-    df = get_tweets()
+    # df = get_tweets()
 
 
-    return render(request,"tweetapp/index.html",{"tw_id":df.to_html()})
+    # return render(request,"tweetapp/index.html",{"tw_id":df.to_html()})
+    return render(request,"tweetapp/index.html",{"tw_id":tw_id})
