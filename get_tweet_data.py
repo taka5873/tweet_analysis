94286a5c03f687a10c2cd0f20516b4fe4aa73ce7
takamasa <taka@takamasas-MacBook-Air.local>
2020-02-05 14:31:57 +0900

    ツイート表示

--- get_tweet_data.py
+++ get_tweet_data.py
@@ -1,33 +1,31 @@
-# import tweepy
-# import pandas as pd
-# import datetime
-#
-# # TweepyAPI KEY
-# CONSUMER_KEY = "czH2MU9FpfQJlaCOkXGgG6NOF"
-# CONSUMER_SECRET = "bQ5l0t7ypCkARf7PDdSWqWeogAi38emDoPb3UJChuasgvtYNLB"
-# ACCESS_TOKEN = "1067433150970654720-yiord2TbDdGjPto1KzqTv1lbQeJHtl"
-# ACCESS_TOKEN_SECRET = "D2wCmoiTDIdqYDIpbZFo32lPv3a7CUmEzsgJ6p5IaYtDL"
-# # tweepyの設定
-# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-# api = tweepy.API(auth)
-# columns_name = ["TW_NO", "TW_TIME", "TW_TEXT", "FAV", "RT"]
-# # ここで取得したいツイッターアカウントIDを指定する
+import pandas as pd
+import tweepy
+import datetime
+
+# TweepyAPI KEY
+CONSUMER_KEY = "czH2MU9FpfQJlaCOkXGgG6NOF"
+CONSUMER_SECRET = "bQ5l0t7ypCkARf7PDdSWqWeogAi38emDoPb3UJChuasgvtYNLB"
+ACCESS_TOKEN = "1067433150970654720-yiord2TbDdGjPto1KzqTv1lbQeJHtl"
+ACCESS_TOKEN_SECRET = "D2wCmoiTDIdqYDIpbZFo32lPv3a7CUmEzsgJ6p5IaYtDL"
+# tweepyの設定
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+columns_name = ["TW_NO", "TW_TIME", "TW_TEXT", "FAV", "RT"]
+# ここで取得したいツイッターアカウントIDを指定する
 # tw_id = "hayatakun52n5"
-#
-#
-# # ツイート取得
-# def get_tweets():
-#     tweet_data = []
-#     for tweet in tweepy.Cursor(api.user_timeline, screen_name=tw_id, exclude_replies=True).items():
-#         tweet_data.append([tweet.id, tweet.created_at + datetime.timedelta(hours=9), tweet.text.replace('\n', ''),
-#                            tweet.favorite_count, tweet.retweet_count])
-#     df = pd.DataFrame(tweet_data, columns=columns_name)
-#     return df
-#
-#     df.to_csv("./tweet_data1.csv")
-#
-#     print("end1")
-#
-#
-# get_tweets()
+
+
+# ツイート取得
+def get_tweets(tw_id):
+    tweet_data = []
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name=tw_id, exclude_replies=True).items(20):
+        tweet_data.append([tweet.id, tweet.created_at + datetime.timedelta(hours=9), tweet.text.replace('\n', ''),
+                           tweet.favorite_count, tweet.retweet_count])
+    df = pd.DataFrame(tweet_data, columns=columns_name)
+    return df
+
+    # df.to_csv("./tweet_data1.csv")
+
+    # print("end1")
+
--- tweetapp/views.py
+++ tweetapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from get_tweet_data import get_tweets
+from get_tweet_data import get_tweets
 
 # Create your views here.
 
@@ -9,8 +9,8 @@ def index(request):
 
 def get_tweet_id(request):
     tw_id = request.POST["tweet_id"]
-    # df = get_tweets()
+    df = get_tweets(tw_id)
 
 
-    # return render(request,"tweetapp/index.html",{"tw_id":df.to_html()})
-    return render(request,"tweetapp/index.html",{"tw_id":tw_id})
+    return render(request,"tweetapp/index.html",{"tw_id":df.to_html()})
+    # return render(request,"tweetapp/index.html",{"tw_id":tw_id})
